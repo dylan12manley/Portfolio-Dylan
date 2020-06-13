9a61383bb0f878374ebccfa354c038bce73d74bf
Dylan Manley <dylan.manley@gmail.com>
2020-06-12 19:07:56 -0700

    add background gradient to landing page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import Landing from './pages/landing/Landing'
+import Home from './pages/home/Home'
+import { AnimatedSwitch } from 'react-router-transition'
+import './App.css'
 
 class App extends React.Component {
 
@@ -8,9 +11,15 @@ class App extends React.Component {
     return (
       <div>
       <Router>
-        <Switch>
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className="switch-wrapper"
+        >
           <Route exact path="/" component={Landing}/>
-        </Switch>
+          <Route path="/home" component={Home}/>
+        </AnimatedSwitch>
       </Router>
       </div>
     );
