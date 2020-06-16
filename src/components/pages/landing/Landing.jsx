42389e92017dd87c4f03b8286391efa566a60666
Dylan Manley <dylan.manley@gmail.com>
2020-06-16 05:39:36 -0700

    add wild on hover transformations on landing page

--- src/components/pages/landing/Landing.jsx
+++ src/components/pages/landing/Landing.jsx
@@ -1,53 +1,57 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Landing.scss'
-import D from '../../../assets/images/D-green.png'
-import L from '../../../assets/images/L-green.png'
-import N from '../../../assets/images/N-green.png'
+import D from '../../../assets/images/d-black.png'
+import Y from '../../../assets/images/y-black.png'
+import L from '../../../assets/images/l-black.png'
+import A from '../../../assets/images/a-black.png'
+import N from '../../../assets/images/n-black.png'
+import M from '../../../assets/images/m-black.png'
+import E from '../../../assets/images/e-black.png'
 
 function Landing(){
 
-    function mouseLetter(e) {
-        e.target.style.transform = 'rotate(-1.2deg)'
-        e.target.style.filter= 'brightness(1.2)'
+    function mouseA(e) {
+        e.target.style.transform = 'rotate(-12deg)'
+        e.target.style.width= '24vw'
+      }
+      function mouseB(e) {
+        e.target.style.transform = 'rotate(54deg)'
+      }
+      function mouseC(e) {
+        e.target.style.transform = 'rotate(272deg)'
         e.target.style.height= '33vmin'
+      }
+      function mouseD(e) {
+        e.target.style.transform = 'rotate(3deg)'
+        e.target.style.height= '19vh'
       }
     
       function clickLogo(e) {
         e.target.style.transform = 'rotate(-90deg)'
-        
       }
 
   return (
     <div className='landing'>
-      <div className='letters'>
+      <div className='first-name'>
         <Link to="/home">
-          <img  
-            src={D} 
-            alt='D' 
-            onMouseOver={mouseLetter}
-            className='dln'
-            onClick={clickLogo}
-          >
-          </img>
-          <img  
-            src={L} 
-            alt='L' 
-            onMouseOver={mouseLetter}
-            className='dln'
-            onClick={clickLogo}
-          >
-          </img>
-          <img  
-            src={N} 
-            alt='N' 
-            onMouseOver={mouseLetter}
-            className='dln'
-            onClick={clickLogo}
-          >
-          </img>
+          <img src={D} alt='D' onMouseOver={mouseA} className='dylan-imgs' onClick={clickLogo}></img>
+          <img src={Y} alt='Y' onMouseOver={mouseB} className='dylan-imgs' onClick={clickLogo}></img>
+          <img src={L} alt='L' onMouseOver={mouseA}className='dylan-imgs' onClick={clickLogo}></img>
+          <img src={A} alt='A' onMouseOver={mouseB}className='dylan-imgs' onClick={clickLogo}></img>
+          <img src={N} alt='N' onMouseOver={mouseA}className='dylan-imgs' onClick={clickLogo}></img>
         </Link>
-      </div>            
+      </div>     
+      <div className='seccond-name'>
+        <Link to="/self">
+          <img src={M} alt='M' onMouseOver={mouseB} className='manley-imgs' onClick={clickLogo}></img>
+          <img src={A} alt='A' onMouseOver={mouseA}className='manley-imgs' onClick={clickLogo}></img>
+          <img src={N} alt='N' onMouseOver={mouseC}className='manley-imgs' onClick={clickLogo}></img>
+          <img src={L} alt='L' onMouseOver={mouseD}className='manley-imgs' onClick={clickLogo}></img>
+          <img src={E} alt='E' onMouseOver={mouseA}className='manley-imgs' onClick={clickLogo}></img>
+          <img src={Y} alt='Y' onMouseOver={mouseB} className='manley-imgs' onClick={clickLogo}></img>  
+        </Link>
+      </div>        
     </div>
   );      
 }
