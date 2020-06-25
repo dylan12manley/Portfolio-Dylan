5ae808fe98574f86fc80d0b9edd89e4de350eb1d
Dylan Manley <dylan.manley@gmail.com>
2020-06-24 19:41:43 -0700

    tighten styling on homepage

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -11,16 +11,15 @@ function Home(){
   return (
     <div id='home'>
       <Header/>
-      <div className='home-body'>  
-        <h2>Home Page</h2> 
-        <a class='resume' href='https://www.resume.com/in/dylan-manley-pdx/'>
-          <img src={resume} alt='resume link'></img>  
+      <div className='home-grid'>  
+        <a class='g-itm' href='https://docs.google.com/document/d/e/2PACX-1vSkXF3AJwzjDb-8uZRGe42nQ631_SMA2-4QOzmxm9GkmKcJmkn46sBhWyd_NlTUEQdcg6w_3tQ9pvHR/pub'>
+          <img class='resume' src={resume} alt='resume link'></img>  
         </a>     
-        <a class='github' href='https://github.com/dylan12manley'>
-          <img src={github} alt='github link'></img>  
+        <a class='g-itm' href='https://github.com/dylan12manley'>
+          <img class='github' src={github} alt='github link'></img>  
         </a>     
-        <a class='linkedin' href='https://www.linkedin.com/in/dylan-manley-pdx/'>
-          <img src={linkedIn} alt='linkedin link'></img>  
+        <a class='g-itm' href='https://www.linkedin.com/in/dylan-manley-pdx/'>
+          <img class='linkedin' src={linkedIn} alt='linkedin link'></img>  
         </a>     
       </div>
     </div>
