9608f15177957527b0fc0e71e8e76170bec5cac6
Dylan Manley <dylan.manley@gmail.com>
2020-10-08 18:48:42 -0700

    alter the order images are displayed on the home page

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../shared/header/Header'
 import './Home.scss'
 import github from '../../../assets/images/github.png'
-import linkedIn from '../../../assets/images/linkedin.png'
+import starrats from '../../../assets/images/starrats.png'
 import resume from '../../../assets/images/resume.png'
 
 function Home(){
@@ -11,22 +11,19 @@ function Home(){
   return (
     <div id='home'>
       <Header/>
-      <div id='home-top'>
-      </div>
-      <div className='home-grid'>  
-        <div> 
-          
+        <div className='home-grid'>  
+            <a class='g-itm' href='https://docs.google.com/document/d/e/2PACX-1vSkXF3AJwzjDb-8uZRGe42nQ631_SMA2-4QOzmxm9GkmKcJmkn46sBhWyd_NlTUEQdcg6w_3tQ9pvHR/pub'>
+              <img class='resume' src={resume} alt='resume link'></img>  
+            </a> 
+            <a class='g-itm' href='https://github.com/dylan12manley'>
+              <img class='github' src={github} alt='github link'></img>  
+            </a>          
         </div>
-        <a class='g-itm' href='https://docs.google.com/document/d/e/2PACX-1vSkXF3AJwzjDb-8uZRGe42nQ631_SMA2-4QOzmxm9GkmKcJmkn46sBhWyd_NlTUEQdcg6w_3tQ9pvHR/pub'>
-          <img class='resume' src={resume} alt='resume link'></img>  
-        </a>     
-        <a class='g-itm' href='https://github.com/dylan12manley'>
-          <img class='github' src={github} alt='github link'></img>  
-        </a>     
-        <a class='g-itm' href='https://www.linkedin.com/in/dylan-manley-pdx/'>
-          <img class='linkedin' src={linkedIn} alt='linkedin link'></img>  
-        </a>     
-      </div>
+        <div> 
+          <a class='g-itm' href='https://www.linkedin.com/in/dylan-manley-pdx/'>
+            <img class='starrats' src={starrats} alt='starrats link'></img>  
+          </a>  
+        </div>
     </div>
   );      
 }
