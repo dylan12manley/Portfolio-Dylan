9d2f669e22aad35edd18f6874ccf46a0ee822c81
dylan12manley <dylan.manley@gmail.com>
2021-04-15 16:34:59 -0700

    replace starrtats image with programing icon

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../shared/header/Header'
 import './Home.scss'
 import github from '../../../assets/images/github.png'
-import starrats from '../../../assets/images/starrats.png'
+import programing from '../../../assets/images/programing.png'
 import resume from '../../../assets/images/resume.png'
 
 function Home(){
@@ -11,19 +11,17 @@ function Home(){
   return (
     <div id='home'>
       <Header/>
-        <div className='home-grid'>  
-          <a class='g-itm' href='https://docs.google.com/document/d/e/2PACX-1vSkXF3AJwzjDb-8uZRGe42nQ631_SMA2-4QOzmxm9GkmKcJmkn46sBhWyd_NlTUEQdcg6w_3tQ9pvHR/pub'>
-            <img class='resume' src={resume} alt='resume link'></img>  
-          </a> 
-          <a class='g-itm' href='https://github.com/dylan12manley'>
-            <img class='github' src={github} alt='github link'></img>  
-          </a>          
-        </div>
-        <div> 
-          <a class='g-itm' href='https://www.linkedin.com/in/dylan-manley-pdx/'>
-            <img class='starrats' src={starrats} alt='starrats link'></img>  
-          </a>  
-        </div>
+      <div className='home-grid'>  
+        <a id='programing' class='g-itm' href='https://www.linkedin.com/in/dylan-manley-pdx/'>
+          <img class='programing' src={programing} alt='programing link'></img>  
+        </a>  
+        <a id='resume' class='g-itm' href='https://docs.google.com/document/d/e/2PACX-1vSkXF3AJwzjDb-8uZRGe42nQ631_SMA2-4QOzmxm9GkmKcJmkn46sBhWyd_NlTUEQdcg6w_3tQ9pvHR/pub'>
+          <img class='resume' src={resume} alt='resume link'></img>  
+        </a> 
+        <a id='github' class='g-itm' href='https://github.com/dylan12manley'>
+          <img class='github' src={github} alt='github link'></img>  
+        </a>          
+      </div>
     </div>
   );      
 }
