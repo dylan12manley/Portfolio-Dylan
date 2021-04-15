10955c5d2327694e63ce525632c05f274155c2c5
dylan12manley <dylan.manley@gmail.com>
2021-04-15 15:39:24 -0700

    Fix the sizing of the name on the header

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -12,12 +12,12 @@ function Home(){
     <div id='home'>
       <Header/>
         <div className='home-grid'>  
-            <a class='g-itm' href='https://docs.google.com/document/d/e/2PACX-1vSkXF3AJwzjDb-8uZRGe42nQ631_SMA2-4QOzmxm9GkmKcJmkn46sBhWyd_NlTUEQdcg6w_3tQ9pvHR/pub'>
-              <img class='resume' src={resume} alt='resume link'></img>  
-            </a> 
-            <a class='g-itm' href='https://github.com/dylan12manley'>
-              <img class='github' src={github} alt='github link'></img>  
-            </a>          
+          <a class='g-itm' href='https://docs.google.com/document/d/e/2PACX-1vSkXF3AJwzjDb-8uZRGe42nQ631_SMA2-4QOzmxm9GkmKcJmkn46sBhWyd_NlTUEQdcg6w_3tQ9pvHR/pub'>
+            <img class='resume' src={resume} alt='resume link'></img>  
+          </a> 
+          <a class='g-itm' href='https://github.com/dylan12manley'>
+            <img class='github' src={github} alt='github link'></img>  
+          </a>          
         </div>
         <div> 
           <a class='g-itm' href='https://www.linkedin.com/in/dylan-manley-pdx/'>
