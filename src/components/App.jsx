efc9f64fbbaf4d3f7a3e5c4a9e0dd8fda392de89
dylan12manley <dylan.manley@gmail.com>
2021-04-21 15:41:53 -0700

    remove animated switch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Landing from './pages/landing/Landing'
 import Home from './pages/home/Home'
 import Code from './pages/code/Code'
 import Music from './pages/music/Music'
 import Self from './pages/self/Self'
-import { AnimatedSwitch } from 'react-router-transition'
 import './App.css'
 
 class App extends React.Component {
@@ -14,13 +13,13 @@ class App extends React.Component {
     return (
       <div id='app-body'>
       <Router>
-        <AnimatedSwitch atEnter={{ opacity: 0 }} atLeave={{ opacity: 0 }} atActive={{ opacity: 1 }} className="switch-wrapper">
+        <Switch>
           <Route exact path="/" component={Landing}/>
           <Route path="/home" component={Home}/>
           <Route path="/code" component={Code}/>
           <Route path="/music" component={Music}/>
           <Route path="/self" component={Self}/>
-        </AnimatedSwitch>
+        </Switch>
       </Router>
       </div>
     );
