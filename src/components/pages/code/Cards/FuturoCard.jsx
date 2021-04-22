d3b27073157e3a442fc5e88cabd0e37bc88faf72
dylan12manley <dylan.manley@gmail.com>
2021-04-22 16:38:17 -0700

    create card and transfer content

---
+++ src/components/pages/code/Cards/FuturoCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+import futuro from '../../../../assets/images/futuro.jpg'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function FuturoCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={futuro}
+          title="Futuro Magazine"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Futuro Magazine
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Made independently, using React, Redux, Javascript, and Firebase. Futuro Magazinbe is a digital platform, promoting culture and fashion. Made for a small company based out of Melborn Austrailia. <a href='https://futuromagazine-be1ee.firebaseapp.com/'>Futuro Magazine</a>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
