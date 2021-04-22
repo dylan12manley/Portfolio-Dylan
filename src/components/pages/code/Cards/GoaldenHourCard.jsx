511efbd8b3d6fc8350f1a26a2baffc7fc100cb32
dylan12manley <dylan.manley@gmail.com>
2021-04-22 16:38:27 -0700

    create cards and transfer content

---
+++ src/components/pages/code/Cards/GoaldenHourCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+
+import goalden from '../../../../assets/images/goalden-logo.png'
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+export default function FuturoCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={goalden}
+          title="Goalden Hour"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Hillcrest Sports
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+          Made with a BigCommerce template using Javascript, Handlebars, jQuery,and more. It’s  an E-commerce site for an outdoor sports shop in Gresham Oregon. <a href='https://hillcrestsports.com/'>Hillcrest Sports</a>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
