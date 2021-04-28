6eba656e85e67c50692cba225f0746ae8a3b8edb
dylan12manley <dylan.manley@gmail.com>
2021-04-28 15:00:40 -0700

    add extended content

--- src/components/pages/code/Cards/HillcrestCard.jsx
+++ src/components/pages/code/Cards/HillcrestCard.jsx
@@ -7,6 +7,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 import hillcrest from '../../../../assets/images/hillcrest.png'
+import hillcrest2 from '../../../../assets/images/hillcrest2.png'
+import hillcrest3 from '../../../../assets/images/hillcrest3.png'
 
 const useStyles = makeStyles({
   root: {
@@ -26,15 +28,31 @@ export default function FuturoCard() {
         <CardMedia
           className={classes.media}
           image={hillcrest}
-          title="Futuro Magazine"
+          title="Hillcrest Ski and Sports"
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             Hillcrest Sports
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          Made with a partner, using a BigCommerce template using Javascript, Handlebars, jQuery,and more. It’s  an E-commerce site for an outdoor sports shop in Gresham Oregon. <a href='https://hillcrestsports.com/'>Hillcrest Sports</a>
-          </Typography>
+          <p>
+          Made with a Robbie Cunningham, using a BigCommerce template with Javascript, Handlebars, React, jQuery,and more. 
+          </p>
+        </CardContent>
+        <CardMedia
+          className={classes.media}
+          image={hillcrest2}
+          title="Hillcrest Homepage"
+        />
+        <CardContent>
+          <p>This is an E-commerce site for an outdoor sports shop in Gresham Oregon. This project also included a custom script, made in Python. The script runs automatically every 15 minutes and syncs the phyisical stores invintory, using Retail-Pro, with our website as well as the store's postings on third party sites like Ebay and sideline swap.</p>
+        </CardContent>
+        <CardMedia
+          className={classes.media}
+          image={hillcrest3}
+          title="Futuro Products"
+        />
+        <CardContent>
+          <p><a href='https://hillcrestsports.com/'>Visit Hillcrest Sports</a></p>
         </CardContent>
       </CardActionArea>
     </Card>
