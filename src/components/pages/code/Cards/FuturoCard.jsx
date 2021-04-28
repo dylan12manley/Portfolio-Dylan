6107e2593eda10a43c6ccd68acb3b6ec29ee1237
dylan12manley <dylan.manley@gmail.com>
2021-04-27 18:42:06 -0700

    import stylesheet

--- src/components/pages/code/Cards/FuturoCard.jsx
+++ src/components/pages/code/Cards/FuturoCard.jsx
@@ -5,6 +5,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import './Card.scss'
 
 import futuro from '../../../../assets/images/futuro.jpg'
 
@@ -33,7 +34,7 @@ export default function FuturoCard() {
             Futuro Magazine
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
-          Made independently, using React, Redux, Javascript, and Firebase. Futuro Magazinbe is a digital platform, promoting culture and fashion. Made for a small company based out of Melborn Austrailia. <a href='https://futuromagazine-be1ee.firebaseapp.com/'>Futuro Magazine</a>
+          Made independently, using React, Redux, Javascript, and Firebase. Futuro Magazine is a digital platform, promoting culture, art and fashion. Made for a Joey Paradox, based out of Melborn Austrailia. <a href='https://futuromagazine-be1ee.firebaseapp.com/'>Futuro Magazine</a>
           </Typography>
         </CardContent>
       </CardActionArea>
