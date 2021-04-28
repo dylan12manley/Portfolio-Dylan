c537111a968019fd0d58c852b2fbd878dabac6a9
dylan12manley <dylan.manley@gmail.com>
2021-04-27 19:09:28 -0700

    add images and a longer description

--- src/components/pages/code/Cards/FuturoCard.jsx
+++ src/components/pages/code/Cards/FuturoCard.jsx
@@ -8,6 +8,8 @@ import Typography from '@material-ui/core/Typography';
 import './Card.scss'
 
 import futuro from '../../../../assets/images/futuro.jpg'
+import futuro2 from '../../../../assets/images/futuro-2.png'
+import futuro3 from '../../../../assets/images/futuro-3.png'
 
 const useStyles = makeStyles({
   root: {
@@ -15,6 +17,9 @@ const useStyles = makeStyles({
   },
   media: {
     height: 140,
+  },
+  img2: {
+    height: 240,
   },
 });
 
@@ -33,9 +38,26 @@ export default function FuturoCard() {
           <Typography gutterBottom variant="h5" component="h2">
             Futuro Magazine
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          Made independently, using React, Redux, Javascript, and Firebase. Futuro Magazine is a digital platform, promoting culture, art and fashion. Made for a Joey Paradox, based out of Melborn Austrailia. <a href='https://futuromagazine-be1ee.firebaseapp.com/'>Futuro Magazine</a>
-          </Typography>
+          <p>
+          Made independently, using React, Redux, Javascript, and Firebase. Futuro Magazine is a digital platform, promoting culture, art and fashion.  
+          </p>
+        </CardContent>
+        <CardMedia
+          className={classes.img2}
+          image={futuro2}
+          title="Futuro Article"
+        />
+        <CardContent>
+          <p>This was my final independent project for Epicodus. I made it for an old friend, Joesoph Hoover, based out of Melborn, Austrailia. </p>
+          <p> I have also created a seccond site for the writers portal. There is a login/sign up option, I created using firebase's authintication. The portal mirrors the main site but has extra features for users with special privilages, like access to custom forms used to create the articals and advertisements on the site.</p>
+        </CardContent>
+        <CardMedia
+          className={classes.img2}
+          image={futuro3}
+          title="Futuro Article Bottom"
+        />
+        <CardContent>
+          <a href='https://futuromagazine-be1ee.firebaseapp.com/'>Futuro Magazine</a>
         </CardContent>
       </CardActionArea>
     </Card>
