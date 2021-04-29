3c5004f9d9a5ab3487dae89a1aa40a1a5541d452
dylan12manley <dylan.manley@gmail.com>
2021-04-29 14:37:20 -0700

    fix spacing

--- src/components/pages/code/Cards/FuturoCard.jsx
+++ src/components/pages/code/Cards/FuturoCard.jsx
@@ -4,9 +4,7 @@ import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
 import './Card.scss'
-
 import futuro from '../../../../assets/images/futuro.jpg'
 import futuro2 from '../../../../assets/images/futuro-2.png'
 import futuro3 from '../../../../assets/images/futuro-3.png'
@@ -39,7 +37,7 @@ export default function FuturoCard() {
           <h2>
             Futuro Magazine
           </h2>
-          <p>
+          <p className='card-p'>
           Made independently, using React, Redux, Javascript, and Firebase. Futuro Magazine is a digital platform, promoting culture, art, and fashion.  
           </p>
         </CardContent>
@@ -49,7 +47,7 @@ export default function FuturoCard() {
           title="Futuro Article"
         />
         <CardContent>
-          <p>This was my final independent project for Epicodus. I made it for an old friend, Joseph Hoover, based out of Melbourne, Australia. </p>
+          <p className='card-p'>This was my final independent project for Epicodus. I made it for an old friend, Joseph Hoover, based out of Melbourne, Australia. </p>
           </CardContent>
           <CardMedia
           className={classes.img2}
@@ -57,7 +55,7 @@ export default function FuturoCard() {
           title="Futuro Article Bottom"
         />
           <CardContent>
-          <p> I have also created a second site for the writers portal. There is a login/sign-up option, I created using firebase's authentication. The portal mirrors the main site but has extra features for users with special privileges, like access to custom forms used to create the articles and advertisements on the site.</p>
+          <p className='card-p'> I have also created a second site for the writers portal. There is a login/<span style={{whiteSpace: 'nowrap'}}>sign-up</span> option, I created using firebase's authentication. The portal mirrors the main site but has extra features for users with special privileges, like access to custom forms used to create the articles and advertisements on the site.</p>
         </CardContent>
         <CardContent>
           <p><a className='card-link' href='https://futuromagazine-be1ee.firebaseapp.com/'>Visit Futuro Magazine</a></p>
