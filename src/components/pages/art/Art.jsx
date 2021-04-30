448b541510045b7daefa63ceede89d17a7dc5abc
dylan12manley <dylan.manley@gmail.com>
2021-04-29 18:58:24 -0700

    adjust content to fit for the Art componet

--- src/components/pages/art/Art.jsx
+++ src/components/pages/art/Art.jsx
@@ -1,32 +1,41 @@
 import React, { useState }  from 'react'
 import Header from '../../shared/header/Header'
 import Footer from '../../shared/footer/Footer'
-import './Music.scss'
+import './Art.scss'
 
 import resume from '../../../assets/images/resume.png'
 
-function Music(){
+function Art(){
 //   const [isShownD, setIsShownD] = useState(false);
 //   const [isShownE, setIsShownE] = useState(false);
 //   const [isShownF, setIsShownF] = useState(false);
   
   return (
-    <div id='music'>
+    <div id='art'>
       <Header/>
-      <div className='music'>  
-        <div className='singles'>
+      <div className='art'>  
+        <div className='art-piece'>
         
         </div>      
-        <div className='rawwar'>
+        <div className='art-piece'>
 
         </div>
-        <div className='all-that-glit'>
+        <div className='art-piece'>
 
-        </div>   
+        </div>
+        <div className='art-piece'>
+        
+        </div>      
+        <div className='art-piece'>
+
+        </div>
+        <div className='art-piece'>
+
+        </div>      
       </div>
       <Footer/>
     </div>
   );      
 }
 
-export default Music
+export default Art
